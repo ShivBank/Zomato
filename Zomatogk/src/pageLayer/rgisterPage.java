d4a36d5df51488a26562c8a7cf6eb1edd6348fdd
Shivraj Bankar <shivrajbankar7887@gmail.com>
2023-04-04 12:34:34 +0530

    ragister page commit
    we created register page

---
+++ Zomatogk/src/pageLayer/rgisterPage.java
@@ -0,0 +1,21 @@
+package pageLayer;
+
+import java.util.Scanner;
+
+public class rgisterPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter full name");
+		
+		String r= sc.next();
+		System.out.println("full name is ="+ r);
+		System.out.println("enter roll no.");
+		int k= sc.nextInt();
+		System.out.println("Roll no. is="+k);
+		
+
+	}
+
+}
