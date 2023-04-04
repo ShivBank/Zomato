48d34904a0b1062819013cb3535c72c27d932610
Shivraj Bankar <shivrajbankar7887@gmail.com>
2023-04-04 12:23:09 +0530

    Login page  commit
    We completed work of login page

---
+++ Zomatogk/src/pageLayer/LoginPage.java
@@ -0,0 +1,16 @@
+package pageLayer;
+
+import java.util.Scanner;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+	
+		Scanner kunal= new Scanner(System.in);
+		System.out.println("enter your name");
+		String a=kunal.next();
+		System.out.println("your good name is= "+ a);
+
+	}
+
+}
